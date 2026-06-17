3c33a7e85c12c2e961f685678efeef37ed0bd343
Rhuan Pablo <rhuan.p.f@hotmail.com>
2026-06-16 21:08:11 -0300

    feat: Initialized the Versioning and Serilog.

--- src/FinalidadeEstudo.API/Program.cs
+++ src/FinalidadeEstudo.API/Program.cs
@@ -2,15 +2,14 @@ using FinalidadeEstudo.CrossCutting.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 
 builder.Services.AddInfrastructureServices(builder.Configuration)
                 .AddApplicationServices()
-                .AddDependencyInjection();
+                .AddDependencyInjection()
+                .AddLoggingServices(builder.Configuration);
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
